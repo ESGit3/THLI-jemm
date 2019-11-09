4602a89e0a9e95842b517f1c6e2061cc1459cb69
ESGit3 <ethanshan706@gmail.com>
2019-11-09 18:48:04 -0500

    fuck my ass im actually dying

--- Main.py
+++ Main.py
@@ -1,14 +1,16 @@
-from kivy import App
+from kivy.app import App
 from kivy.uix.button import Button
 
+uscores = 0 #this will be the total points assigned per 25min and reset when user quits the program completely
 
 class TestApp(App):
     @staticmethod
     def build():
         return Button(text='Hello World')
 
-
 TestApp().run()
+
+
 # taskkill
 def killProcess(taskName):
     os.system("taskkill /f /im " + taskName)
@@ -22,11 +24,32 @@ cluster = MongoClient("mongodb+srv://jkska23:<teenhacksdb>@cluster0-ctd6l.mongod
 db = cluster["teenhacks"]
 collection = db[""]
 
+def uinfo():
+    q = input("Would you like to sign up?: (Y/N) \n")
+    if q == "Y":
+        uid = input("Enter your nickname: \n")
+        umail = input("Enter your email: \n")
+        upass = input("Enter your password: \n")
+        post = {"_id": uid, "email": umail, "password": upass}
+        user.insert_one(post)
+    else:
+        print("You can sign in with your gmail to submit your points")
 
 
-# # study, example
-# post = {"_id": 1, "user": user1, "password": 12345, "score": 0}  # make it the same structure as dictionaries
-# users.insert_one(post)
+
+
+
+
+
+def ucinfo(): #this is for chaning into that i can work on later
+    q = input("Would you like to change your information?: (Y/N) \n")
+    if q == "Y":
+        print("something")
+    else:
+        print("You can always access this information and change your information")
+
+
+
 #
 # existQ = collection.find({"name": bill})
 # for i in existQ:
