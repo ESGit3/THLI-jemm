592c33bc65cacf98b0d3f86e61b9c40e7e8b7e19
ESGit3 <ethanshan706@gmail.com>
2019-11-09 16:35:37 -0500

    work pls

--- Main.py
+++ Main.py
@@ -7,7 +7,6 @@ import tkinter as tkt
 import time
 import os
 
-
 top = tkt.tk()
 window.title("Pomodoro")
 window.geometry('1250x350')
@@ -59,20 +58,19 @@ def killProcess(taskName):
 
 killProcess("/Applications/Discord.app")
 
-#mongodb
+# mongodb
 cluster = MongoClient("mongodb+srv://jkska23:<teenhacksdb>@cluster0-ctd6l.mongodb.net/test?retryWrites=true&w=majority")
 
-db = cluster ["teenhacks"]
+db = cluster["teenhacks"]
 collection = db[""]
 
-
-#study, example
-post = {"_id": 1, "user": user1, "password": 12345, "score": 0} #make it the same structure as dictionaries
+# study, example
+post = {"_id": 1, "user": user1, "password": 12345, "score": 0}  # make it the same structure as dictionaries
 users.insert_one(post)
 
 existQ = collection.find({"name": bill})
 for i in existQ:
-    print(i["_id"]) #gives the ID of the user with name bill
+    print(i["_id"])  # gives the ID of the user with name bill
 
-#i just put this here to make it easier for me to pull
-#git pull https://github.com/ESGit3/THLI-jemm.git
+# i just put this here to make it easier for me to pull
+# git pull https://github.com/ESGit3/THLI-jemm.git
