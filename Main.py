4c6f8564528418d0edd2fcaa2e42e398f90394c7
jkska23 <hanmin@hanmin.local>
2019-11-09 13:45:29 -0500

    x

--- Main.py
+++ Main.py
@@ -4,24 +4,37 @@ import pymongo
 import tkinter as tkt
 import time
 
-#gui
+
 top = tkt.tk()
 window.title("Procrastination")
 window.geometry('1250x350')
 
-btn1 = Button(Window, text = "Start", command = clicked)
-btn2 = Button(Window, text = "Stop", command = clicked)
+#logins
+un = tk.Label(window, text = "Username").grid(row = 0)
+tk.Entry(window).grid(row = 0, column = 1)
+passw = tk.Label(window, text = "Password").grid(row = 1)
+tk.Entry(window).grid(row = 1, column = 1)
+tk.Checkbutton(window, text = "Keep Me Logged In").grid(columnspan = 2)
 
+btn1 = Button(Window, text = "Start", command = clicked)
+btn2 = Button(Window, text = "Stop", command = clickQuit).pack
 
 combo = Combobox(window)
 combo['Values'] = ("10 min": 10, "20 min": 20, "30 min": 30, "40 min": 40, "50 min": 50, "60 min": 60)
 combo.grid (column = 0, row = 0)
 
-
+def clickStart():
+    btn1.configure(text = "")
+def clickQuit():
+    btn2.configure(text = "Would you like to quit?\n If you quit now your points will not be awarded")
+    quit()
 top.mainloop()
 
-def clicked():
-    btn1.configure(text = "")
-def clicked():
-    btn2.configure(text = "Would you like to quit?\n If you quit now your points will not be awarded")
+
+
+
+
+
+
+window.mainloop()
 
