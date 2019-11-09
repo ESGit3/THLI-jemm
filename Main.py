f21ff848e1070627ad2e69006ed736d98801b7ff
jkska23 <hanmin@hanmin.local>
2019-11-09 16:30:59 -0500

    god damn it fucking work

--- Main.py
+++ Main.py
@@ -59,4 +59,20 @@ def killProcess(taskName):
 
 killProcess("/Applications/Discord.app")
 
-# mongodb+srv://jkska23:<password>@cluster0-ctd6l.mongodb.net/test?retryWrites=true&w=majority
+#mongodb
+cluster = MongoClient("mongodb+srv://jkska23:<teenhacksdb>@cluster0-ctd6l.mongodb.net/test?retryWrites=true&w=majority")
+
+db = cluster ["teenhacks"]
+collection = db[""]
+
+
+#study, example
+post = {"_id": 1, "user": user1, "password": 12345, "score": 0} #make it the same structure as dictionaries
+users.insert_one(post)
+
+existQ = collection.find({"name": bill})
+for i in existQ:
+    print(i["_id"]) #gives the ID of the user with name bill
+
+#i just put this here to make it easier for me to pull
+#git pull https://github.com/ESGit3/THLI-jemm.git
