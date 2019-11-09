6b4a35ec794d8c11c7e87e9763b657e03e302e06
Ethan Shan <ethanshan@Ethans-MacBook-Pro-6.local>
2019-11-09 12:46:00 -0500

    x

--- Main.py
+++ Main.py
@@ -1,7 +1,19 @@
-import os
+import pymongo
+import tkinter as tkt
+import time
 
-def killProcess(name):
-    os.system("taskkill /f /im " + name)
+#gui
+top = tkt.tk()
+window.title("Procrastination")
+window.geometry('1250x350')
 
+qe = Button(Window, text = "Stop", command = clicked)
 
-killProcess("Discord.app")
+combo = Combobox(window)
+combo['Values'] = ("10 min": 10, "20 min": 20, "30 min": 30, "40 min": 40, "50 min": 50, "60 min": 60)
+combo.grid (column = 0, row = 0)
+
+top.mainloop()
+
+def clicked():
+    qe.configure(text = "Would you like to quit?\n If you quit now your points will not be awarded")
