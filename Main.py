cb555f46d5fe3dca8a9017c9e00cc08f91376e74
Ethan Shan <ethanshan@Ethans-MacBook-Pro-6.local>
2019-11-09 12:59:07 -0500

    x

--- Main.py
+++ Main.py
@@ -7,13 +7,19 @@ top = tkt.tk()
 window.title("Procrastination")
 window.geometry('1250x350')
 
-qe = Button(Window, text = "Stop", command = clicked)
+btn1 = Button(Window, text = "Start", command = clicked)
+btn2 = Button(Window, text = "Stop", command = clicked)
+
 
 combo = Combobox(window)
 combo['Values'] = ("10 min": 10, "20 min": 20, "30 min": 30, "40 min": 40, "50 min": 50, "60 min": 60)
 combo.grid (column = 0, row = 0)
 
+
 top.mainloop()
 
 def clicked():
-    qe.configure(text = "Would you like to quit?\n If you quit now your points will not be awarded")
+    btn1.configure(text = "")
+def clicked():
+    btn2.configure(text = "Would you like to quit?\n If you quit now your points will not be awarded")
+
