c885141f189c4a02ef2162441b7e508d916570c2
jkska23 <hanmin@hanmin.local>
2019-11-09 21:10:53 -0500

    s

--- Main.py
+++ Main.py
@@ -5,6 +5,7 @@ from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
+from pymongo import MongoClient
 
 import time
 
