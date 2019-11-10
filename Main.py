ec7ecaf994aad914aa30300bc8a48a37f112f76b
Miguel Quiroz <miguelblob@gmail.com>
2019-11-09 20:11:25 -0500

    gay

--- Main.py
+++ Main.py
@@ -1,5 +1,9 @@
-from kivy.app import App
 from kivy.uix.button import Button
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+
 
 uscores = 0 #this will be the total points assigned per 25min and reset when user quits the program completely
 
@@ -7,7 +11,29 @@ class TestApp(App):
     @staticmethod
     def build():
         return Button(text='Hello World')
+###
+class LoginScreen(GridLayout):
 
+    def __init__(self, **kwargs):
+        super(LoginScreen, self).__init__(**kwargs)
+        self.cols = 2
+        self.add_widget(Label(text='User Name'))
+        self.username = TextInput(multiline=False)
+        self.add_widget(self.username)
+        self.add_widget(Label(text='password'))
+        self.password = TextInput(password=True, multiline=False)
+        self.add_widget(self.password)
+
+
+class MyApp(App):
+
+    def build(self):
+        return LoginScreen()
+
+
+if __name__ == '__main__':
+    MyApp().run()
+###
 TestApp().run()
 
 
